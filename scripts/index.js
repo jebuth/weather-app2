88a36773c92d6f6be2fc6d16d87f9788363f88dd
justinb <justinbuth91@live.com>
2019-02-04 17:00:11 -0800

    started UI module

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// UI Elements Module
+// this module will be responsible for controlling UI elements like 'menu'
+const UI = (function (){
+    showApp = () => {
+        
+    };
+    
+})();
+    
