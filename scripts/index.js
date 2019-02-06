876be8d5c6292e27c5b09d46fabf146b7ec2c2b9
justinb <justinbuth91@live.com>
2019-02-05 16:55:52 -0800

    finished local storge module

--- scripts/index.js
+++ scripts/index.js
@@ -252,6 +252,44 @@ const WEATHER = (function(){
 
 })();
 
+
+// Local Storage API
+// ===================================================
+const LOCALSTORAGE = (function (){
+    let savedCities = [];
+
+    const save = (city) => {
+        savedCities.push(city);
+        localStorage.setItem('savedCities', JSON.stringify(savedCities));
+    }
+
+    const get = () => {
+        if(localStorage.getItem('savedCities') != null)
+            savedCities = JSON.parse(localStorage.getItem('savedCities'));
+    }
+
+    const remove = (index) => {
+        if(index < savedCities.length){
+            savedCities.splice(index, 1);
+            localStorage.setItem('savedCities', JSON.stringify(savedCities));
+        }
+    }
+
+    const getSavedCities = () =>{
+        return savedCities;
+    }
+
+    return{
+        save, 
+        get,
+        remove,
+        getSavedCities
+    }
+
+})();
+
+
+
 // Init
 // ===================================================
 window.onload = function () {
